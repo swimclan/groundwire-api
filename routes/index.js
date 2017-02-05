9d88a4be532e7e860ab5f9af1f74135393d9ed74
Matthew Herron <matthew.herron77@gmail.com>
2017-02-05 12:02:29 -0600

    working on more trading methods allow buys with either inst ID or symbol

--- routes/index.js
+++ routes/index.js
@@ -1,134 +1,108 @@
 var express = require('express');
 var router = express.Router();
-var Robinhood = require('robinhood');
-var credentials = require('../credentials');
 var bodyParser = require('body-parser');
-
-
-/* HELPERS */
-var sendJSONResponse = function(status, res, body) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(status).send(JSON.stringify(body));
-}
-
-var getInstrument = function(ticker) {
-	return new Promise(function(resolve, reject) {
-		var rh = Robinhood(credentials, function() {
-		    rh.instruments(ticker, function(err, response, instrument) {
-			    if (err) {
-			    	console.log(err);
-			    	reject(err);
-			    }
-			    console.log(`Instrument for ${ticker} is`);
-			    console.log(instrument);
-			    var retInst = null;
-			    instrument.results.forEach(function(inst) {
-			    	if (inst.symbol === ticker) {
-			    		retInst = inst;		
-			    	}
-			    });
-			    resolve(retInst);
-			});
-		});
-	});
-}
-
-var placeBuy = function(options) {
-	return new Promise(function(resolve, reject) {
-		var rh = Robinhood(credentials, function() {
-			rh.place_buy_order(options, function(error, response, body) {
-			    if(error) {
-			        console.error(error);
-			        reject(error);
-			    } else {
-			        resolve(body);
-			    }
-			});
-		});
-	});
-}
-
-getProfile = function() {
-	return new Promise(function(resolve, reject) {
-		var rh = Robinhood(credentials, function() {
-		    rh.investment_profile(function(err, response, body) {
-		    	if (err) {
-		    		console.log(err);
-		    		reject(err);
-		    	}
-		    	//console.log(body);
-		    	resolve(body);
-		    });
-		});
-	});
-}
-
-var getPrice = function(ticker) {
-	return new Promise(function(resolve, reject) {
-		var rh = Robinhood(credentials, function() {
-		    rh.quote_data(ticker, function(error, response, body) {
-		        if (error) {
-		            console.error(error);
-		            reject(error); 
-		        }
-		        var priceResults = body.results;
-		        var retPrice = null;
-		        priceResults.forEach(function(result) {
-		        	if (result.symbol === ticker) {
-		        		retPrice = result;
-		        	}
-		        });
-		     	resolve(retPrice);
-		    });
-		});
-	});
-}
-
-var validAPIKey = function(req) {
-	return req.query.key === process.env.API_KEY;
-}
+var trade = require('../lib/trading');
+var utils = require('../lib/utils');
 
 /* ROUTES */
 /* GET investor profile */
 router.get('/', function(req, res, next) {
-	if (!validAPIKey(req)) return sendJSONResponse(401, res, { error: "Unauthorized: Invalid or no API key provided" });
+	if (!trade.validAPIKey(req)) return utils.sendJSONResponse(401, res, { error: "Unauthorized: Invalid or no API key provided" });
 	console.log("Getting investor profile");
-	getProfile()
+	trade.getProfile()
 	.then(function(profile) {
-		sendJSONResponse(200, res, profile);
+		utils.sendJSONResponse(200, res, profile);
 	})
 	.catch(function(err) {
-		sendJSONResponse(500, res, { error: err });
+		utils.sendJSONResponse(500, res, { error: err });
+	});
+});
+
+/* GET user profile */
+router.get('/user', function(req, res, next) {
+	console.log("Getting user profile");
+	trade.getUser()
+	.then(function(user) {
+		utils.sendJSONResponse(200, res, user);
+	})
+	.catch(function(err) {
+		utils.sendJSONResponse(500, res, { error: err });
+	});
+});
+
+/* GET user account */
+router.get('/accounts', function(req, res, next) {
+	console.log("Getting user account");
+	trade.getAccounts()
+	.then(function(account) {
+		utils.sendJSONResponse(200, res, account);
+	})
+	.catch(function(err) {
+		utils.sendJSONResponse(500, res, { error: err });
+	});
+});
+
+/* GET positions */
+router.get('/positions', function(req, res, next) {
+	console.log("getting positions");
+	trade.getPositions()
+	.then(function(positions) {
+		utils.sendJSONResponse(200, res, positions);
+	})
+	.catch(function(err) {
+		utils.sendJSONResponse(500, res, err);
 	});
 });
 
 /* GET ticker price */
 router.get('/price/:ticker', function(req, res, next) {
-	if (!validAPIKey(req)) return sendJSONResponse(401, res, { error: "Unauthorized: Invalid or no API key provided" });
+	if (!trade.validAPIKey(req)) return utils.sendJSONResponse(401, res, { error: "Unauthorized: Invalid or no API key provided" });
 	var ticker = req.params.ticker;
 	console.log(`Getting price data for: ${ticker} `);
-	getPrice(ticker)
+	trade.getPrice(ticker)
  	.then(function(data) {
- 		sendJSONResponse(200, res, data);	
+ 		utils.sendJSONResponse(200, res, data);	
 	})
 	.catch(function(err) {
-		sendJSONResponse(500, res, { error: err })
+		utils.sendJSONResponse(500, res, { error: err })
+	});
+});
+
+/* GET watchlists */
+router.get('/watchlist', function(req, res, next) {
+	trade.getWatchList()
+	.then(function(watchlist) {
+		console.log(watchlist);
+		utils.sendJSONResponse(200, res, watchlist);
+	})
+	.catch(function(err) {
+		utils.sendJSONResponse(500, res, { error: err });
 	});
 });
 
 /* POST place buy order  */
 router.post('/buy', bodyParser.json(), function(req, res, next) {
-	if (!validAPIKey(req)) return sendJSONResponse(401, res, { error: "Unauthorized: Invalid or no API key provided" });
+	if (!trade.validAPIKey(req)) return utils.sendJSONResponse(401, res, { error: "Unauthorized: Invalid or no API key provided" });
 	var reqBody = req.body;
-	if (Object.keys(reqBody).indexOf("quantity") == -1 || Object.keys(reqBody).indexOf("ticker") == -1) {
-		return sendJSONResponse(400, res, { error: { message: "Need all valid fields for request" } });
+	if (Object.keys(reqBody).indexOf("quantity") === -1) {
+		return utils.sendJSONResponse(400, res, { error: { message: "Must specify a quantity" } });
+	} 
+	if (Object.keys(reqBody).indexOf("symbol") === -1 && Object.keys(reqBody).indexOf("instrumentId") === -1) {
+		return utils.sendJSONResponse(400, res, { error: { message: "Must specify either a symbol or an instrument ID" } });	
+	}
+	if (Object.keys(reqBody).indexOf("symbol") !== -1) {
+		var instQueryType = 'symbol';
+		var instQuery = reqBody.symbol;
+		var quantity = reqBody.quantity;
 	} else {
-		var ticker = reqBody.ticker;
+		var instQueryType = 'instrument';
+		var instQuery = reqBody.instrumentId;
 		var quantity = reqBody.quantity;
 	}
-	getInstrument(ticker)
+	var instrumentQueryMethod = instQueryType === 'symbol' ? trade.getInstrumentFromTicker : trade.getInstrumentFromUrl
+	instrumentQueryMethod(instQuery)
 	.then(function(inst) {
-		getPrice(inst.symbol)
+		trade.getPrice(inst.symbol)
 		.then(function(data) {
 			console.log(`Price data for ${inst.symbol} is: `);
 			console.log(data);
@@ -143,50 +117,24 @@ router.post('/buy', bodyParser.json(), function(req, res, next) {
 		            symbol: inst.symbol
 		        }
 		    }
-		    placeBuy(options)
+		    trade.placeBuy(options)
 		    .then(function(buy) {
-		    	sendJSONResponse(200, res, buy);
+		    	utils.sendJSONResponse(200, res, buy);
 		    })
 		    .catch(function(err) {
 		    	console.log(err);
-		    	sendJSONResponse(500, res, { error: err })
+		    	utils.sendJSONResponse(500, res, { error: err })
 		    });
 		})
 		.catch(function(err) {
 			console.log(err);
-			sendJSONResponse(500, res, { error: err });
+			utils.sendJSONResponse(500, res, { error: err });
 		});
 	})
 	.catch(function(err){
 		console.log(err);
-		sendJSONResponse(500, res, { error: err });
+		utils.sendJSONResponse(500, res, { error: err });
 	});
-
-	// var rh = Robinhood(credentials, function() {
-	//     rh.instruments('GEVO', function(err, response, instrument) {
-	// 	    if (err) {
-	// 	    	console.log(err);
-	// 	    	process.exit(1);
-	// 	    }
-	// 	    console.log(instrument.results[0].url);
-	// 	    var options = {
-	// 	        type: 'limit',
-	// 	        quantity: 20,
-	// 	        bid_price: 3.00,
-	// 	        instrument: {
-	// 	            url: instrument.results[0].url,
-	// 	            symbol: instrument.results[0].symbol
-	// 	        }
-	// 	    }
-	// 		rh.place_buy_order(options, function(error, response, body) {
-	// 		    if(error){
-	// 		        console.error(error);
-	// 		    } else{
-	// 		        console.log(body);
-	// 		    }
-	// 		});
-	//     });
-	// });
 });
 
 module.exports = router;
