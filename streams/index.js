621db57a7bdbca2bd3499de122a4210dc1826335
Matthew Herron <matthew.herron77@gmail.com>
2017-03-23 17:45:35 -0500

    Added yahoo delayed quotes support

--- streams/index.js
+++ streams/index.js
@@ -1,16 +1,24 @@
 var trade = require('../lib/trading');
 
+var intervals = {};
+
 module.exports = function(server, io) {
-    io.listen(server).sockets.on('connection', function(socket) {
+    var socketsServer = io.listen(server);
+    socketsServer.sockets.on('connection', function(socket) {
         console.log('Welcome to the GroundWire stock price stream');
-        setInterval(function() {
+        intervals.polling = setInterval(function() {
             trade.getYahooPrice('GEVO')
             .then(function(data) {
-               socket.emit('price', { price: data }) 
+                console.log('Sending to client: $' + data.price + ' traded at ' + data.time);
+                socket.emit('price', data);
             })
             .catch(function(err){
                 console.log(error);
             });
         }, 3000);
+        socket.on('disconnect', function() {
+            clearInterval(intervals.polling);
+            console.log('Thank you for using the GroundWire stock price stream');
+    });
     });
 }
