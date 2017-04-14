e678f0f9782d14af253ef736d0d213709c7942f3
Matthew Herron <matthew.herron77@gmail.com>
2017-04-13 22:31:00 -0500

    Socket server with Intrinio integration

--- streams/index.js
+++ streams/index.js
@@ -1,25 +1,37 @@
-var trade = require('../lib/trading');
-
-var intervals = {};
+'use strict';
+var IntrinioRealtime = require('intrinio-realtime');
+var irCreds = require('../credentials/intrinio');
+var utils = require('../lib/utils');
 
 module.exports = function(server, io) {
     var socketsServer = io.listen(server);
     socketsServer.sockets.on('connection', function(socket) {
-        var ticker = socket.handshake.query ? socket.handshake.query.ticker : "NULL";
-        socket.join(ticker);
-        intervals.polling = setInterval(function() {
-            trade.getYahooPrice(ticker)
-            .then(function(data) {
-                console.log('Sending to client: $' + data.price + ' traded at ' + data.time);
-                socketsServer.in(ticker).emit('price', data);
-            })
-            .catch(function(err){
-                console.log(error);
-            });
-        }, 3000);
-        socket.on('disconnect', function() {
-            clearInterval(intervals.polling);
+        console.log("New connection detected");
+        console.log("Client id: ", socket.id);
+        let ir = new IntrinioRealtime(irCreds);
+        let request = socket.handshake;
+        var ticker = request.query ? request.query.ticker : "NULL";
+        if (utils.validAPIKey(request)) {
+            socket.join(ticker);
+            ir.join(ticker);
+        } else {
+            console.log("Error: Invalid or no api key provided");
+            console.log("Disconnecting client: ", socket.id);
+            socket.emit('close', 'Error: Invalid or no api key provided');
+            socket.disconnect();
+        }
+
+        ir.onQuote(quote => {
+            var { ticker, type, price, size, timestamp } = quote;
+            console.log("STREAM QUOTE: ", ticker, type, price, size, timestamp);
+            socketsServer.in(ticker).emit('quote', quote);
+        });
+
+        socket.on('disconnect', function(e) {
             console.log('Thank you for using the GroundWire stock price stream');
+            console.log(socket.id, "disconnected");
+            ir.leaveAll();
+            ir.destroy();
         });
     });
 }
